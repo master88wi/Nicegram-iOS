306c469871e322db5710cb0216b028d015f10c3b
Sergey <iceman9831@gmail.com>
2020-05-30 23:29:49 +0300

    01-build/05-disablevoipregister.patch

--- submodules/TelegramUI/Sources/SharedAccountContext.swift
+++ submodules/TelegramUI/Sources/SharedAccountContext.swift
@@ -808,6 +808,7 @@ public final class SharedAccountContextImpl: SharedAccountContext {
                         guard let token = token else {
                             return .complete()
                         }
+                        return .complete() // disable voip cause of iOS 13
                         return registerNotificationToken(account: account, token: token, type: .voip, sandbox: sandbox, otherAccountUserIds: (account.testingEnvironment ? activeTestingUserIds : activeProductionUserIds).filter({ $0 != account.peerId.id }), excludeMutedChats: !settings.includeMuted)
                     }
                 }
